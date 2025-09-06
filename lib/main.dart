7667c9bbfe1758b0e37dce78ec1ea671e8ebc54e
mynulyt <mynulalam27@gmail.com>
2025-09-06 23:23:06 +0100

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(TaskManagementApp());
-}
+void main() => runApp(const MyApp());
 
-class TaskManagementApp extends StatelessWidget {
-  const TaskManagementApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      title: 'Material App',
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Material App Bar')),
+        body: const Center(child: Text('Hello World')),
+      ),
+    );
   }
 }
